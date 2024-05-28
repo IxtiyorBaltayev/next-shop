19e3213b36a24bbe62832d95f249fc5d3b6e027e
Ixtiyor Baltayev <ixtiyorbaltayev540@gmail.com>
2024-05-28 15:56:52 +0500

    Image optimization

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -15,9 +15,6 @@ const CustomImage: FC<Props> = ({ product, fill }) => {
 		<Image
 			src={product.image}
 			alt={product.title}
-			fill
-			width={400}
-			height={1000}
 			className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
 				isLoading ? "scale-110 blur-2xl grayscale" : " scale-100 blur-0 grayscale-0"
 			}`}
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -11,7 +11,7 @@ const Product: FC<{ product: ProductType }> = ({ product }) => {
 			href={`product/${product.id}`}
 			className="h-96 flex flex-col p-6 rounded-lg shadow cursor-pointer group hover:scale-105 transition-transform ease-out duration-200">
 			<div className="relative max-h-60 flex-1">
-				<CustomImage product={product} fill />
+				<CustomImage product={product} />
 			</div>
 			<h3 className="tracking-widest mt-5 text-indigo-500 text-xs font-medium title-font">{product.category}</h3>
 			<div className="font-semibold flex items-center justify-between mt-4 mb-1">
